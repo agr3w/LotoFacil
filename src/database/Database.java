c88048de0bcdc3026d48fdbc609cd68c0d9a74d7
agr3w <suzanakampa.sk@gmail.com>
2024-10-30 17:13:16 -0300

    Alterando o metodo de salvemento de dados dos usuarios.

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,49 +1,47 @@
 package database;
 
 import java.io.*;
-import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Database {
     private static final String FILE_NAME = "usuarios.txt"; // ta criando usuarios com nomes iguais, ARRUMAR
+    private static final String filePath = "c:\\tmp\\" + FILE_NAME;
 
     // Método para verificar credenciais
     public static boolean checkCredentials(String cpf, String senha) {
         try {
-            File file = new File(FILE_NAME);
-            if (!file.exists()) {
-                return false;
-            }
+            Path path = Paths.get(filePath);
+            List<String> lines = Files.readAllLines(path);
 
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+            for (String line : lines) {
                 String[] data = line.split(";");
-                String userCpf = data[0].split(": ")[1]; // Extrair o CPF
-                String userSenha = data[2].split(": ")[1]; // Extrair a senha
-                
-                // Verificar credenciais
+                String userCpf = data[0].split(": ")[1];
+                String userSenha = data[2].split(": ")[1];
+
                 if (userCpf.equals(cpf) && userSenha.equals(senha)) {
-                    scanner.close();
-                    return true; // Credenciais corretas
+                    return true;
                 }
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return false; // Credenciais incorretas
+        return false;
     }
 
     // Método para salvar um novo usuário no arquivo
     public static void saveUser(String nome, String email, String cpf, String Nascimento,String senha) {
+        Path path = Paths.get(filePath);
         try {
-            FileWriter writer = new FileWriter(FILE_NAME, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            Files.createDirectories(path.getParent());
+            BufferedWriter writer = Files.newBufferedWriter(path);
 
-            bufferedWriter.write("CPF: " + cpf + ";" + "DataNascimento: " + Nascimento + ";" + "senha: " + senha + ";" + "nome: " + nome + ";" + "email: " + email);
-            bufferedWriter.newLine(); // Adicionar nova linha para o próximo usuário
-            bufferedWriter.close();
+            writer.write("CPF: " + cpf + ";" + "DataNascimento: " + Nascimento + ";" + "senha: " + senha + ";" + "nome: " + nome + ";" + "email: " + email);
+            writer.newLine(); // Adicionar nova linha para o próximo usuário
+            writer.close();
 
             System.out.println("Usuário salvo: " + nome);
         } catch (IOException e) {
